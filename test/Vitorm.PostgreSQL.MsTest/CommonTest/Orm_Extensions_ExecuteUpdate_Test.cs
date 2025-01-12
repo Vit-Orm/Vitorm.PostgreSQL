d1205b10e1159718cc95ec7eb58e36f4fc5e902c
Lith <LithWang@outlook.com>
2025-01-12 21:51:00 +0800

    2.5.0

--- test/Vitorm.PostgreSQL.MsTest/CommonTest/Orm_Extensions_ExecuteUpdate_Test.cs
+++ test/Vitorm.PostgreSQL.MsTest/CommonTest/Orm_Extensions_ExecuteUpdate_Test.cs
@@ -24,7 +24,7 @@ namespace Vitorm.MsTest.CommonTest
 
                 Assert.AreEqual(6, count);
 
-                var userList = userQuery.ToList();
+                var userList = userQuery.OrderBy(m => m.id).ToList();
                 Assert.AreEqual("u_1_4_6", userList.First().name);
                 Assert.AreEqual(DateTime.Parse("2021-01-11 00:00:00"), userList.First().birth);
                 Assert.AreEqual("u_6__", userList.Last().name);
@@ -47,7 +47,7 @@ namespace Vitorm.MsTest.CommonTest
                 Assert.AreEqual(6, count);
 
 
-                var userList = userQuery.ToList();
+                var userList = userQuery.OrderBy(m => m.id).ToList();
                 Assert.AreEqual("u2_1_4_6", userList.First().name);
                 Assert.AreEqual("u2_6__", userList.Last().name);
             }
