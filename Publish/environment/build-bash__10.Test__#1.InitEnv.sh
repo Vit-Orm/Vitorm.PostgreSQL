0898e8c8f36e79dd50f676b65a82b43ba2c14ce2
Lith <LithWang@outlook.com>
2025-01-12 21:56:40 +0800

    init test sh

--- Publish/environment/build-bash__10.Test__#1.InitEnv.sh
+++ Publish/environment/build-bash__10.Test__#1.InitEnv.sh
@@ -12,7 +12,7 @@ export basePath=/root/temp
 
 
 #---------------------------------------------------------------------
-echo '#build-bash__10.Test__#1.InitEnv.sh -> #1 start ClickHouse container'
+echo '#build-bash__10.Test__#1.InitEnv.sh -> #1 start PostgreSQL container'
 docker rm vitorm-postgres -f || true
 docker run -d --name vitorm-postgres -p 5432:5432 -e POSTGRES_PASSWORD=123456 -e POSTGRES_DB=db_orm -e ALLOW_IP_RANGE=0.0.0.0/0 postgres:15.8
 
@@ -22,7 +22,7 @@ echo '#build-bash__10.Test__#1.InitEnv.sh -> #8 wait for containers to init'
 
 
 echo '#build-bash__10.Test__#1.InitEnv.sh -> #8.1 wait for PostgreSQL to init' 
-docker run -t --rm --link vitorm-postgres postgres:15.8 timeout 120 sh -c "export PGPASSWORD=123456; until psql -h postgres -U postgres -d db_orm -c 'create database db_orm2;'; do echo waiting for PostgreSQL; sleep 2; done;"
+docker run -t --rm --link vitorm-postgres postgres:15.8 timeout 120 sh -c "export PGPASSWORD=123456; until psql -h vitorm-postgres -U postgres -d db_orm -c 'create database db_orm2;'; do echo waiting for PostgreSQL; sleep 2; done;"
 
 
 #---------------------------------------------------------------------
